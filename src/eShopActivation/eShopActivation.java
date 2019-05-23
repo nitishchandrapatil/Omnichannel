0b01ac642e50b003a5075599e82fd6a51e761ef4
npatil <npatil@QINDW173.it.local>
2019-05-23 13:07:10 +0530

    commit

--- src/eShopActivation/eShopActivation.java
+++ src/eShopActivation/eShopActivation.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -32,4 +33,11 @@ public class eShopActivation {
 		
 		
 	}
+	
+	@AfterTest
+	void closeBrowser() {
+		
+		driver.close();
+	}
+	
 }
