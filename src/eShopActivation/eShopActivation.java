b04a07861108f2d0005e7bb6f72798b9698e50f1
Nitish Patil <npatil@qvantel.com>
2019-08-20 16:35:40 +0530

    latest changes on personal details and address information

--- src/eShopActivation/eShopActivation.java
+++ src/eShopActivation/eShopActivation.java
@@ -2,21 +2,35 @@ package eShopActivation;
 
 import static org.testng.Assert.assertTrue;
 
+import java.awt.AWTException;
+import java.awt.Robot;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import java.util.Calendar;
+import java.util.List;
+
+//import com.sun.xml.bind.v2.schemagen.xmlschema.List;
+
 import eShopActivation.fileRead;
 import testDataProvide.testDataprovide;
+
+
 public class eShopActivation extends testDataprovide  {
 	
 	static WebDriver driver;
@@ -30,8 +44,18 @@ public class eShopActivation extends testDataprovide  {
 	public WebElement email;
 	public WebElement confirmEmail;
 	public WebElement contactNumber;
+	public WebElement documentNumber;
+	public WebElement authorization;
+	public WebElement issueDate;
+	public WebElement endDate;
+	public WebElement table;
+	public WebElement street;
+	public WebElement building;
+	public Robot ro;
 	fileRead f;
+	Select se;
 	testDataprovide t1;
+	fillCustomerPersonalDetails fill;
 
 	
 	
@@ -44,9 +68,9 @@ public class eShopActivation extends testDataprovide  {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 	}
-
+	
 	@Test
-	void onboardingFlow() throws InterruptedException, IOException {
+	void onboardingFlow() throws InterruptedException, IOException, AWTException {
 	Thread.sleep(3000);	
 	fileRead f= new fileRead();
 	String str= f.propertyFile();
@@ -86,15 +110,67 @@ public class eShopActivation extends testDataprovide  {
     
     Thread.sleep(2000);
     
-    fillCustomerPersonalDetails fill= new fillCustomerPersonalDetails();
+     fill= new fillCustomerPersonalDetails();
     email = driver.findElement(By.id(fill.getEmailfield()));
     email.sendKeys(t1.cellValue4);
-    confirmEmail=driver.findElement(By.id(fill.getEmailfield_Confirmed()));
-    email.sendKeys(t1.cellValue5);
+    Thread.sleep(2000);
+    confirmEmail=driver.findElement(By.xpath(fill.getEmailfield_Confirmed()));
+    confirmEmail.sendKeys(t1.cellValue5);
     contactNumber = driver.findElement(By.id(fill.getContactphone()));
     contactNumber.sendKeys("3837282992");
+    Thread.sleep(3000);
+    se= new Select(driver.findElement(By.xpath("//select[@class=\"custom-select\" and @id=\"documents-id-type\"]")));
+    se.selectByIndex(1);
+    documentNumber=driver.findElement(By.id("PersonDetailsForm-documents-id-number-field"));
+    documentNumber.sendKeys("S83893984");
+    authorization= driver.findElement(By.id("PersonDetailsForm-documents-authority-field"));
+    authorization.sendKeys("Italia");
+    se= new Select(driver.findElement(By.id("documents-authority-country")));
+    se.selectByVisibleText("Italia");
+    issueDate= driver.findElement(fill.getDoc_startDate());
+    issueDate.click();
+    Thread.sleep(2000);
+    issueDate.sendKeys("02/08/2019");
+    endDate= driver.findElement(fill.getDoc_endDate());
+    endDate.click();
+    endDate.sendKeys("09/08/2029");
     
+    /*table= driver.findElement(By.xpath("//input[@aria-label=\"Data di rilascio\"]"));
+    List<WebElement>row_select= table.findElements(By.tagName("tr"));
+    for(WebElement Row:row_select) {
+    	
+    	List<WebElement> cells=Row.findElements(By.tagName("td"));
+    	
+    	for(WebElement Cell:cells) {
+    		
+    		String s="10";
+    		if(Cell.getText().contentEquals(s))
+    		{
+    			Cell.click();
+    			
+    			
+    		}
+    	}
+    	
+    }*/
+    		
     
+    //issueDate=driver.findElement(By.xpath("//input[@aria-label=\\\"Data di nascita\\\"]"));
+    
+   // SoftAssert soft= new SoftAssert();
+    // soft.assertEquals(arg0, arg1);
+    se= new Select(driver.findElement(By.id("PersonDetailsPostalAddressForm-postalAddress-province-field")));
+    se.selectByVisibleText("Roma RM");
+    se= new Select(driver.findElement(By.id("PersonDetailsPostalAddressForm-postalAddress-city-field")));
+    se.selectByVisibleText("Roma");
+    se= new Select(driver.findElement(By.id("PersonDetailsPostalAddressForm-postalAddress-postalCode-field")));
+    se.selectByVisibleText("00148");
+    street= driver.findElement(fill.getStreetField());
+    street.sendKeys("Via Carlo Veneziani");
+    building= driver.findElement(fill.getBuildingNumber());
+    building.sendKeys("58");
+    WebElement continueButton=driver.findElement(fill.buttonGoToConfiguration);
+    continueButton.click();
 	}
 	
 	
--- src/eShopActivation/fillCustomerPersonalDetails.java
+++ src/eShopActivation/fillCustomerPersonalDetails.java
@@ -1,10 +1,25 @@
 package eShopActivation;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class fillCustomerPersonalDetails {
 	
+	By by;
+	
+	
 	private String email_field = "PersonDetailsForm-email-field";					
-	private String confirm_emailfield= "PersonDetailsForm-confirmation-email-field";			
+	private String confirm_emailfield= "//input[@id=\"PersonDetailsForm-confirmation-email-field\"]";			
 	private String contact_phone="PersonDetailsForm-contact-phone-field";
+	By document_startDate= By.xpath("//input[@aria-label=\"Data di rilascio\"]");
+	By document_endDate= By.xpath("//input[@aria-label=\"Data di scadenza\"]");
+	By streetField= By.id("PersonDetailsPostalAddressForm-postalAddress-street-field");
+	By buildingNumber= By.id("PersonDetailsPostalAddressForm-postalAddress-building-field");
+	By buttonGoToConfiguration=By.id("buttonGoToConfigurationInCheckoutSetup");
+	
+	
+	//private String selectDocument= "//select[@class=\"custom-select\" and @id=\"documents-id-type\"]";
 	
 	public String getEmailfield() {
 				
@@ -21,6 +36,38 @@ public class fillCustomerPersonalDetails {
 	public String getContactphone() {
 		
 		return contact_phone;
+			
 		
 	}
+		
+	public By getDoc_startDate() {
+		
+		return document_startDate;
+		
+	}
+	public By getDoc_endDate() {
+		
+		return document_endDate;
+		
+	}
+	
+	public By getStreetField() {
+		
+		return streetField;
+		
+	}
+	
+	public By getBuildingNumber() {
+		
+		return buildingNumber;
+		
+	}
+	
+	public By isContinueEnabled() {
+		
+		
+		return buttonGoToConfiguration;
+		
+	}
+	
 }
